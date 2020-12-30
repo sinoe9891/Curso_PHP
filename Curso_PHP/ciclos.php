9e264f1a21efa4eb66189d4a01d17a946699ae28
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2020-12-30 00:09:21 -0600

    Ciclos PHP

--- Curso_PHP/ciclos.php
+++ Curso_PHP/ciclos.php
@@ -1,17 +1,17 @@
 <?php
     //Ciclos
 
-    $numero = 1;
+    // $numero = 1;
 
-    while ($numero <= 10) {
-        echo $numero;
-        $numero = $numero + 1;
-    }
+    // while ($numero <= 10) {
+    //     echo $numero;
+    //     $numero = $numero + 1;
+    // }
 
-    do {
-        echo $numero;
-        $numero++;
-    } while ($numero <= 15);
+    // do {
+    //     echo $numero;
+    //     $numero++;
+    // } while ($numero <= 15);
 
     for ($numero=4; $numero <= 10; $numero++) { 
         echo $numero;
