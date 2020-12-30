4f28b9258382a52003832400f77fdb1f5369c735
sinoe9891 <danny_velasquez_cadenas@hotmail.es>
2020-12-29 23:28:40 -0600

    Condicionales

---
+++ Curso_PHP/condicionales.php
@@ -0,0 +1,14 @@
+<?php
+// Condicionales
+
+$numero_uno = 25;
+$numero_dos = 10;
+
+if ($numero_uno < $numero_dos) {
+    echo 'Curso de PHP';
+}else{
+    echo 'Curso de Python';
+}
+
+
+?>
---
+++ Curso_PHP/variables_constantes.php
@@ -0,0 +1,27 @@
+<?php
+
+    // print 'Hola Mundo ';
+    // echo 'Hola Mundo ', 'hola php';
+
+    // Variables
+    $nombre = 'Danny ';
+    echo $nombre;
+
+    $numero = 3.2;
+    echo $numero;
+
+    echo " este número es: $numero ";
+
+    //Constantes
+    $curso = 'php ';
+    echo $curso;
+    $curso = 'python ';
+    echo $curso;
+
+    define('curso','php');
+    echo curso;
+    // define('curso','python');
+    // echo curso;
+
+
+?>
